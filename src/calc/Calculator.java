40559cba775dcee9d28511f2b13f7526ea1f31b7
Ilya Trenin <itrenin@gmail.com>
2022-02-02 18:26:02 +0300

    fix Calculator.java uninitialize result variable

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -2,15 +2,16 @@ package calc;
 public class Calculator {
 
     public int calculate(int a, int b, String operator){
-        int result = 0;
+        int result;
 
         switch (operator){
-            case "+" : result = a + b; break;
-            case "-" : result = a - b; break;
-            case "*" : result = a * b; break;
-            case "/" : result = a / b; break;
-            default: throw new Error("Оператор может быть только: +, -, *, /. Введите правильный оператор");
+            case "+" -> result = a + b;
+            case "-" -> result = a - b;
+            case "*" -> result = a * b;
+            case "/" -> result = a / b;
+            default -> throw new Error("Оператор может быть только: +, -, *, /. Введите правильный оператор");
         }
+
 
         return result;
     }
